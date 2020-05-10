48d0ed9a7fd027574bf520511700a1345829372f
mcorbin <corbin.math@gmail.com>
2020-05-10 23:04:52 +0200

    root healthcheck component

---
+++ healthcheck/root.go
@@ -0,0 +1,54 @@
+package healthcheck
+
+import (
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+// Healthcheck is the face for an healthcheck
+type Healthcheck interface {
+	Initialize() error
+	Start() error
+	Stop() error
+	Execute() error
+	LogDebug(message string)
+	LogError(err error, message string)
+}
+
+// Component is the component which will manage healthchecks
+type Component struct {
+	Logger       *zap.Logger
+	Healthchecks map[string]Healthcheck
+}
+
+// New creates a new Healthcheck component
+func New(logger *zap.Logger) (*Component, error) {
+	component := Component{
+		Logger:       logger,
+		Healthchecks: make(map[string]Healthcheck),
+	}
+
+	return &component, nil
+
+}
+
+// Start start the healthcheck component
+func (c *Component) Start() error {
+	c.Logger.Info("Starting the healthcheck component")
+	// nothing to do
+	return nil
+}
+
+// Stop stop the healthcheck component, stopping all healthchecks being executed.
+func (c *Component) Stop() error {
+	c.Logger.Info("Stopping the healthcheck component")
+	for _, healthcheck := range c.Healthchecks {
+		healthcheck.LogDebug("stopping healthcheck")
+		err := healthcheck.Stop()
+		if err != nil {
+			healthcheck.LogError(err, "Fail to stop the healthcheck")
+			return errors.Wrap(err, "Fail to stop the healthcheck component")
+		}
+	}
+	return nil
+}
