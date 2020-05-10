792fdfdf47519039ccbc1400ad4da2c51b0452b7
mcorbin <corbin.math@gmail.com>
2020-05-10 22:42:13 +0200

    DNS healthcheck

---
+++ healthcheck/dns.go
@@ -0,0 +1,84 @@
+package healthcheck
+
+import (
+	"time"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"net"
+
+	"gopkg.in/tomb.v2"
+)
+
+// DNSHealthcheckConfiguration defines a DNS healthcheck configuration
+type DNSHealthcheckConfiguration struct {
+	Name        string
+	Description string
+	Domain      string
+	Interval    time.Duration
+	OneOff      bool
+}
+
+// DNSHealthcheck defines an HTTP healthcheck
+type DNSHealthcheck struct {
+	Logger *zap.Logger
+	ID     string
+	config *DNSHealthcheckConfiguration
+	URL    string
+
+	Tick *time.Ticker
+	t    tomb.Tomb
+}
+
+// Start an Healthcheck, which will be periodically executed after a
+// given interval of time
+func (h *DNSHealthcheck) Start() error {
+	h.Tick = time.NewTicker(time.Duration(h.config.Interval))
+	h.t.Go(func() error {
+		for {
+			select {
+			case <-h.Tick.C:
+				h.Execute()
+			case <-h.t.Dying():
+				return nil
+			}
+		}
+	})
+	return nil
+}
+
+// logError logs an error with context
+func (h *DNSHealthcheck) logError(err error, message string) {
+	h.Logger.Error(err.Error(),
+		zap.String("extra", message),
+		zap.String("domain", h.config.Domain),
+		zap.String("name", h.config.Name),
+		zap.String("id", h.ID))
+}
+
+// logError logs a message with context
+func (h *DNSHealthcheck) logDebug(message string) {
+	h.Logger.Debug(message,
+		zap.String("domain", h.config.Domain),
+		zap.String("name", h.config.Name),
+		zap.String("id", h.ID))
+}
+
+// Stop an Healthcheck
+func (h *DNSHealthcheck) Stop() error {
+	h.Tick.Stop()
+	h.t.Kill(nil)
+	h.t.Wait()
+	return nil
+
+}
+
+// Execute executes an healthcheck on the given domain
+func (h *DNSHealthcheck) Execute() error {
+	h.logDebug("start executing healthcheck")
+	_, err := net.LookupIP(h.config.Domain)
+	if err != nil {
+		return errors.Wrapf(err, "Fail to lookup IP for domain")
+	}
+	return nil
+}
---
+++ healthcheck/dns_test.go
@@ -0,0 +1,36 @@
+package healthcheck
+
+import (
+	"testing"
+
+	"go.uber.org/zap"
+)
+
+func TestDNSExecuteSuccess(t *testing.T) {
+	h := DNSHealthcheck{
+		Logger: zap.NewExample(),
+		config: &DNSHealthcheckConfiguration{
+			// it will hopefully resolve ^^
+			Domain: "mcorbin.fr",
+		},
+	}
+
+	err := h.Execute()
+	if err != nil {
+		t.Errorf("healthcheck error :\n%v", err)
+	}
+}
+
+func TestDNSExecuteFailure(t *testing.T) {
+	h := DNSHealthcheck{
+		Logger: zap.NewExample(),
+		config: &DNSHealthcheckConfiguration{
+			Domain: "doesnotexist.mcorbin.fr",
+		},
+	}
+
+	err := h.Execute()
+	if err == nil {
+		t.Errorf("Was expecting an error: the domain does not exist")
+	}
+}
