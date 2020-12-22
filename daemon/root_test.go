8909a1c883c4ac2457de29aa20f8f7827b934d1b
mcorbin <corbin.math@gmail.com>
2020-12-22 18:10:34 +0100

    Add more tests on reload

--- daemon/root_test.go
+++ daemon/root_test.go
@@ -128,6 +128,36 @@ func TestReload(t *testing.T) {
 	if len(component.Healthcheck.ListChecks()) != 3 {
 		t.Fatalf("The healthcheck was not added correctly")
 	}
+	dnsCheck := healthcheck.NewDNSHealthcheck(zap.NewExample(),
+		&healthcheck.DNSHealthcheckConfiguration{
+			Name:     "new-dns-check",
+			Interval: healthcheck.Duration(time.Second * 10),
+		})
+	err = component.Healthcheck.AddCheck(dnsCheck)
+	if err != nil {
+		t.Fatalf("Fail to add dns healthcheck")
+	}
+	if len(component.Healthcheck.ListChecks()) != 4 {
+		t.Fatalf("The DNS healthcheck was not added correctly")
+	}
+	err = component.Reload(&Configuration{
+		HTTP: http.Configuration{
+			Host: "127.0.0.2",
+			Port: 2002,
+		},
+	})
+	if component.Config.HTTP.Host != "127.0.0.2" {
+		t.Fatal("Invalid HTTP address after reload")
+	}
+	if err != nil {
+		t.Fatalf("Fail to reload the component\n%v", err)
+	}
+	if len(component.Healthcheck.ListChecks()) != 1 {
+		t.Fatalf("Only one check should exists")
+	}
+	if component.Healthcheck.ListChecks()[0].Name() != "new-dns-check" {
+		t.Fatalf("Invalid name for the remaining healthcheck")
+	}
 	err = component.Stop()
 	if err != nil {
 		t.Fatalf("Fail to start the component\n%v", err)
