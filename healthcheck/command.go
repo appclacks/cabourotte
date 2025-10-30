25fdd634ca127935d2bb0328b6238eaef216f358
Eléonore Bommart <ebommart@free-mobile.fr>
2025-10-30 15:41:19 +0100

    remove minimum interval requirements

--- healthcheck/command.go
+++ healthcheck/command.go
@@ -41,9 +41,6 @@ func (config *CommandHealthcheckConfiguration) Validate() error {
 		return errors.New("The healthcheck timeout is missing")
 	}
 	if !config.Base.OneOff {
-		if config.Base.Interval < Duration(2*time.Second) {
-			return errors.New("The healthcheck interval should be greater than 2 second")
-		}
 		if config.Base.Interval < config.Timeout {
 			return errors.New("The healthcheck interval should be greater than the timeout")
 		}
